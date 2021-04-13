a2ce5bb68c69a5b58214a0a909e2e998c8995187
WISUNGWON <tkrhkaos12@naver.com>
2021-04-13 20:51:28 +0900

    210413-resolve-Longest-Common-Prefix

--- LeetCode/Longest-Common-Prefix.js
+++ LeetCode/Longest-Common-Prefix.js
@@ -1,35 +1,22 @@
 let longestCommonPrefix = function (strs) {
-  if (strs === undefined) {
-    return '';
+  let res = '';
+
+  if (strs.length === 0) {
+    return res;
   }
-  const newStrs = strs.filter((s) => s !== undefined);
-  let minLen = Number.MAX_SAFE_INTEGER;
-  let minLenIdx = 0;
-  for (let i = 1; i < newStrs.length; i++) {
-    if (newStrs[i].length < minLen) {
-      minLen = newStrs[i].length;
-      minLenIdx = i;
-      break;
-    }
-  }
-  let flag = true;
-  let res = [];
-  let now = '';
-  loop1: for (let i = 0; i < minLen; i++) {
-    let ch = newStrs[minLenIdx].slice(i, i + 1);
-    for (let j = 0; j < newStrs.length; j++) {
-      if (newStrs[j].slice(i, i + 1) !== ch) {
-        flag = false;
-        break loop1;
+
+  let word = strs[0];
+  let Idx = 0;
+  for (ch of word) {
+    for (let i = 1; i < strs.length; i++) {
+      let conWord = strs[i];
+      if (ch !== conWord[Idx]) {
+        return res;
       }
-      now = newStrs[j].slice(i, i + 1);
     }
-    res.push(now);
+    res += ch;
+    Idx++;
   }
 
-  if (res) {
-    return res.join('');
-  }
-
-  return '';
+  return res;
 };
