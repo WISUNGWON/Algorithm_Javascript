975193373f2db284e7a295a8114e4329713f8419
Sungwon WI <52841715+WISUNGWON@users.noreply.github.com>
2021-05-29 16:49:06 +0900

    Solve LeetCode/plusOne

---
+++ LeetCode/Plus-One.js
@@ -0,0 +1,16 @@
+let plusOne = function (digits) {
+  let len = digits.length;
+  for (let i = len - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    }
+
+    digits[i] = 0;
+  }
+
+  digits.unshift(1);
+  return digits;
+};
+
+// Comment : 최대한 내장 함수 사용하지 않고 구현하기!
