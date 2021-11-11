50d7921031fb44b29cf29b2c98602177e31f3f63
WISUNGWON <tkrhkaos12@naver.com>
2021-11-11 20:19:19 +0900

    modify: sort-array-by-increasing-frequency (leetcode)

--- LeetCode/sort-array-by-increasing-frequency.js
+++ LeetCode/sort-array-by-increasing-frequency.js
@@ -1,22 +1,19 @@
-var frequencySort = function (nums) {
-  const map = new Map();
-  nums.forEach((val) => {
-    map.set(val, (map.get(val) || 0) + 1);
-  });
-  const arr = Array.from(map, ([num, cnt]) => ({ num, cnt }));
-  const answer = arr
-    .sort((a, b) => {
-      if (a.cnt > b.cnt) return 1;
-      if (a.cnt < b.cnt) return -1;
-      if (a.num > b.num) return -1;
-      if (a.num < b.num) return 1;
-      return 0;
-    })
-    .map((val) => new Array(val.cnt).fill(val.num))
-    .flat();
+let frequencySort = function (nums) {
+    const map = new Map();
+    nums.forEach((val) => {
+        map.set(val, (map.get(val) || 0) + 1);
+    });
+    const arr = Array.from(map, ([num, cnt]) => ({ num, cnt }));
+    const answer = arr
+        .sort((a, b) => {
+            if (a.cnt > b.cnt) return 1;
+            if (a.cnt < b.cnt) return -1;
+            if (a.num > b.num) return -1;
+            if (a.num < b.num) return 1;
+            return 0;
+        })
+        .map((val) => new Array(val.cnt).fill(val.num))
+        .flat();
 
-  return answer;
+    return answer;
 };
-
-frequencySort([1, 1, 2, 2, 2, 3]);
-frequencySort([-1, 1, -6, 4, 5, -6, 1, 4, 1]);
