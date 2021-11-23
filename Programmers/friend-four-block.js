4fa9f93770e4aa7759ddec3db803d572e4a6a494
WISUNGWON <tkrhkaos12@naver.com>
2021-11-23 23:11:08 +0900

    solve : 프렌즈4블록 (수정), 예상 대진표

--- Programmers/friend-four-block.js
+++ Programmers/friend-four-block.js
@@ -1,53 +1,60 @@
+// 프로그래머스 : 프렌즈4블록
+
 function solution(m, n, board) {
     var answer = 0;
-    // 1) 2차원 배열 다 돌면서 2 x 2 구역 찾기.
-    let newBoard = board.map((str) => str.split(""));
-    let deletedArr = [];
-    while (true) {
-        for (let y = 0; y < newBoard.length - 1; y++) {
-            const rows = newBoard[y];
-            for (let x = 0; x < rows.length - 1; x++) {
-                const curF = rows[x];
-                // 2 x 2 구역 판단
-                if (curF.charCodeAt(0) <= "Z".charCodeAt(0) && curF.charCodeAt(0) >= "A".charCodeAt(0) && curF !== "" && curF === newBoard[y][x + 1] && curF === newBoard[y + 1][x] && curF === newBoard[y + 1][x + 1]) {
-                    // 1-1) 찾은 구역의 인덱스를 x, y로 배열로 만들어서 저장
-                    deletedArr.push([{ y, x }, { y: y + 1, x }, { y, x: x + 1 }, { y: y + 1, x: x + 1 }])
-                }
-            }
+    // 1. board를 2차원 배열로 변환(boardArr)
+    let boardArr = board.map((str) => str.split(""));
+    const checkCanMakeFourBlock = (arr, friend, x, y) => {
+        if (friend !== "" && friend === arr[x][y + 1] && friend === arr[x + 1][y] && friend === arr[x + 1][y + 1]) {
+            return true
         }
-        if (deletedArr.length === 0) break;
-        // 2) 찾은 2 x 2 구역 지우기.
-        deletedArr.forEach((posArr) => {
-            posArr.forEach((pos) => {
-                newBoard[pos.y][pos.x] = "";
-            })
-        })
-        // 3) 배열 다시 만들기.
-        // 3-1) 배열을 row를 기준으로 하나씩 잡고, 요소가 있으면 빈공간으로 넣는 시행을 다 함.
-        let secondBoard = [...newBoard];
-        for (let i = 0; i < newBoard[0].length; i++) {
-            for (let j = 0; j < newBoard.length; j++) {
-                const curF = secondBoard[j][i];
-                if (curF === "") {
-                    let newJ = j;
-                    while(newJ - 1 >= 0 && newJ - 1 !== "") {
-                        newJ--;
-                        let temp = secondBoard[newJ][i];
-                        secondBoard[newJ][i] = "";
-                        secondBoard[newJ + 1][i] = temp;
-                        
+        return false;
+    }
+    const DropBlockToEmptySpace = (arr) => {
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < m; j++) {
+                const friend = arr[j][i];
+                if (friend === "") {
+                    let z = j;
+                    while(z - 1 >= 0 && z - 1 !== "") {
+                        z--;
+                        let temp = arr[z][i];
+                        arr[z][i] = "";
+                        arr[z + 1][i] = temp;
                     }
                 }
             }
         }
-        newBoard = secondBoard;
+    }
+    let deletedArr = [];
+    while (true) {
+    // 2. 변환된 배열을 0,0 -> m - 1, n - 1까지 순회하면서 4블록을 만들 수 있는 경우를 체크
+        for (let x = 0; x < m - 1; x++) {
+            for (let y = 0; y < n - 1; y++) {
+                const friend = boardArr[x][y];
+                if (checkCanMakeFourBlock(boardArr, friend, x, y)) {
+                // 3. 체크된 범위를 객체화하여 지워질 배열(deletedArr)에 저장
+                    deletedArr.push([{ x, y }, { x: x + 1, y }, { x, y: y + 1 }, { x: x + 1, y: y + 1 }])
+                }
+            }
+        }
+        if (deletedArr.length === 0) break;
+        // 4. deletedArr를 순회하면서 boardArr의 공간을 비움
+        deletedArr.forEach((posArr) => {
+            posArr.forEach((pos) => {
+                boardArr[pos.x][pos.y] = "";
+            })
+        })
+        // 5. 비워진 공간에 따라 프렌즈들을 빈 공간으로 내림 (DropBlockToEmptySpace)
+        DropBlockToEmptySpace(boardArr)
         deletedArr = [];
+        // 6. 1 ~ 5의 시행을 더이상 지울 4블록이 없을 때 까지 시행 (deletedArr가 empty일 때)
     }
 
-    //4) newBoard에서 빈 값 카운트
-    newBoard.forEach((board) => {
-        board.forEach((fri) => {
-            if (fri === "") answer++;
+    // 7. 최종적으로 만들어진 boardArr를 순회하면서 지워진 부분 (빈스트링인 부분)을 카운트
+    boardArr.forEach((board) => {
+        board.forEach((friend) => {
+            if (friend === "") answer++;
         })
     })
     return answer;
